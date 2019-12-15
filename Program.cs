1081a91eff24fe5021c09dcd01d1cbc26145019e
xamichoch <xamichoch@mail.ru>
2019-12-15 18:53:00 +0100

    VsCode Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WebWindows.Blazor;
+
+namespace BlazorLinux
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ComponentsDesktop.Run<Startup>("My Blazor App", "wwwroot/index.html");
+        }
+        
+    }
+        
+}
